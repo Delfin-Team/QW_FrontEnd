6870dbbd027e638a6bbe191fb34dc03dc82f1027
Janeth <janeth.ballesteros.olivas97@gmail.com>
2017-07-20 12:43:09 -0500

    Interfaz utilizando vuejs

---
+++ js/app.js
@@ -0,0 +1,3 @@
+Vue.component('mynavbar',{
+  template: '<nav><div class="nav-wrapper indigo accent-3"><a href="index.html" class="brand-logo"> &nbsp Pregunta de la semana</a><a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a><ul class="right hide-on-med-and-down"><li><a href="login.html">Iniciar sesión</a></li><li><a href="register.html">Registro</a></li></ul><ul class="side-nav" id="mobile-demo"><li><a href="login.html">Iniciar sesión</a></li><li><a href="register.html">Registro</a></li></ul></div></nav>',
+});
